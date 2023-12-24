2515cea6457151a828e27d1b52d6995845d1fb50
ivan <ivanartista96@gmail.com>
2023-12-24 09:43:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl bg-red-300 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
